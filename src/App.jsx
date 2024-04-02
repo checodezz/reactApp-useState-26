ac73c365113c137449afa2fb3a3cc92cdbd38130
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-02 17:38:51 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+const TravelDestination = () => {
+  return (
+    <div>
+      <h1>Travel Destination App</h1>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <main>
+      <TravelDestination />
+    </main>
+  );
+}
