f8f740e6be555bcd0c487b892efa6fb59320e141
Chethan Kumar S V <53275990+checodezz@users.noreply.github.com>
2024-04-03 06:04:50 +0000

    Add music preference app
    it is a part of this 2 in one app

--- src/App.jsx
+++ src/App.jsx
@@ -86,10 +86,89 @@ const TravelDestination = () => {
   );
 };
 
+const MusicPreference = () => {
+  const [currentSelectedGenre, setCurrentSelectedGenre] = useState("");
+  const [currentSubGenre, setCurrentSubGenre] = useState("");
+  const [results, setResults] = useState(false);
+
+  const questions = {
+    question: "Which type of music do you prefer?",
+    options: {
+      Rock: ["Classic Rock", "Alternative Rock", "Indie Rock"],
+      Pop: ["Pop Rock", "Synth-pop", "Electropop"],
+      "Hip Hop": ["East Coast Hip Hop", "West Coast Hip Hop", "Trap Music"],
+    },
+  };
+
+  const handleGenreChangeEvent = () => {
+    const value = event.target.value;
+    setCurrentSelectedGenre(value);
+    console.log(currentSelectedGenre);
+  };
+
+  const handleSubGenreChange = () => {
+    const value = event.target.value;
+    setCurrentSubGenre(value);
+    console.log(currentSubGenre);
+  };
+
+  const handleButtonClick = () => {
+    setResults(true);
+  };
+
+  return (
+    <div>
+      <h1>Music Preference App</h1>
+      {!results && (
+        <>
+          <label>{questions.question}:</label>
+          <select onChange={handleGenreChangeEvent}>
+            <option value="">--Select Genre--</option>
+            {Object.keys(questions.options).map((option) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <br />
+          <br />
+          {currentSelectedGenre && (
+            <div>
+              <label>Select your Preferred Subgenre:</label>
+              <br />
+              {questions.options[currentSelectedGenre].map((genre) => (
+                <label key={genre}>
+                  <input
+                    type="radio"
+                    value={genre}
+                    name="subGenre"
+                    onChange={handleSubGenreChange}
+                  />
+                  {genre}
+                  <br />
+                </label>
+              ))}
+            </div>
+          )}
+          <button onClick={handleButtonClick}>Submit</button>
+        </>
+      )}
+      {results && (
+        <div>
+          <h3>Thank you for Sharing your preferences!</h3>
+          <p>Selected Genre: {currentSelectedGenre}</p>
+          <p>Selected Sub Genre: {currentSubGenre}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default function App() {
   return (
     <main>
       <TravelDestination />
+      <MusicPreference />
     </main>
   );
 }
